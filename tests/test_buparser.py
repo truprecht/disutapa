aecd424519dc1f430c1cb07460e81fd9d1210933
Thomas Ruprecht <thomas.ruprecht@tu-dresden.de>
2022-11-03 13:23:22 +0100

    rm lc parser

--- tests/test_buparser.py
+++ tests/test_buparser.py
@@ -1,9 +1,53 @@
 from sdcp.grammar.sdcp import rule, grammar
-from sdcp.grammar.buparser import BuParser
+from sdcp.grammar.buparser import BuParser, BitSpan
 from sdcp.grammar.extract import extract
 from sdcp.corpus import corpus_extractor
 from sdcp.autotree import AutoTree
 from random import sample, randint, shuffle
+
+def test_bitspans():
+    assert BitSpan.fromit([1,2,3], len=10).union(BitSpan.fromit([], len=10)) == BitSpan.fromit([1,2,3], len=10)
+    assert BitSpan.fromit([1,2,3], len=10).union(BitSpan.fromit([4,5,6], len=10)) == BitSpan.fromit(range(1,7), len=10)
+    assert BitSpan.fromit([1,2,3], len=10).union(BitSpan.fromit([5,6], len=10)) == BitSpan.fromit([1,2,3,5,6], len=10)
+    assert BitSpan.fromit([1,2,3,5,6], len=10).union(BitSpan.fromit([4], len=10)) == BitSpan.fromit(range(1,7), len=10)
+    
+    assert BitSpan.fromit([0,3,4,5], len=10).isdisjoint(BitSpan.fromit([1,2], len=10))
+    assert BitSpan.fromit([0,3,4,5], len=10).union(BitSpan.fromit([1,2], len=10)) == BitSpan.fromit(range(6), len=10)
+
+    assert BitSpan.fromit([1,2,3,5,6], len=10).leftmost == 1
+    assert BitSpan.fromit([0,3,4,5], len=10).leftmost == 0
+
+    assert BitSpan.fromit([1,2,3,4], len=10).numgaps() == 0
+    assert BitSpan.fromit([1,2,3,6], len=10).numgaps() == 1
+
+    for l in range(1, 91, 5):
+        for i in range(randint(3, 10)):
+            top = randint(l, l+100)
+            pos1 = sample(range(top), l)
+            pos2 = sample(range(top), l)
+            spans1 = BitSpan.fromit(pos1, len=top)
+            spans2 = BitSpan.fromit(pos2, len=top)
+            set1 = frozenset(pos1)
+            set2 = frozenset(pos2)
+
+            assert list(spans1) == sorted(pos1)
+            assert list(spans2) == sorted(pos2)
+
+            assert spans1.isdisjoint(spans2) == set1.isdisjoint(set2)
+            assert spans2.isdisjoint(spans1) == set2.isdisjoint(set1)
+
+            if not spans1.isdisjoint(spans2):
+                disp2 = BitSpan.fromit((p2 for p2 in pos2 if not p2 in pos1), len=top)
+                sisp2 = frozenset(p2 for p2 in pos2 if not p2 in pos1)
+                assert set(spans1.union(disp2)) == set1.union(sisp2)
+            else:
+                assert set(spans1.union(spans2)) == set1.union(set2)
+
+            assert spans1.firstgap == next(i for i in range(min(set1), max(set1)+2) if not i in set1)
+            assert spans2.firstgap == next(i for i in range(min(set2), max(set2)+2) if not i in set2)
+
+            assert spans1.leftmost == min(pos1)
+            assert spans2.leftmost == min(pos2)
 
 
 def test_lc_parser():

2ecef845894b00bd72fac17337a987fc7f3b7f08
Thomas Ruprecht <thomas.ruprecht@tu-dresden.de>
2023-03-01 15:28:23 +0100

    implement smoothing prior for second-order weights

--- sdcp/tagging/parsing_scorer.py
+++ sdcp/tagging/parsing_scorer.py
@@ -2,29 +2,49 @@ from .data import DatasetWrapper
 from collections import defaultdict
 import torch as t
 from math import log
+from ..grammar.sdcp import rule
 
 class CombinatorialParsingScorer:
-    def __init__(self, corpus: DatasetWrapper):
+    def __init__(self, corpus: DatasetWrapper, prior: int = 1):
         combinations: dict[tuple[int], int] = defaultdict(lambda: 0)
-        denominator: dict[tuple[int], int] = defaultdict(lambda: 0)
+        self.denominator: dict[tuple[int], int] = defaultdict(lambda: 0)
+        self.cnt_by_rhs = defaultdict(lambda: 0)
+        self.rhs = []
+        self.prior = prior
+
+        for i, supertag in enumerate(corpus.labels()):
+            sobj: rule = eval(supertag)
+            if sobj.rhs:
+                self.cnt_by_rhs[sobj.rhs] += 1
+            self.rhs.append(sobj.rhs)
+        self.rhs = tuple(self.rhs)
+
         for sentence in corpus:
             deriv = sentence.get_derivation()
             for node in deriv.subtrees():
                 if not node.children:
                     continue
                 combinations[(node.label[0], *(c.label[0] for c in node))] += 1
-                denominator[tuple(c.label[0] for c in node)] += 1
+                self.denominator[tuple(c.label[0] for c in node)] += 1
+        
         self.probs = {
-            comb: -log(combinations[comb] / denominator[comb[1:]])
+            comb: -log((combinations[comb] + prior) / self.denom(comb[0], comb[1:]))
             for comb in combinations
         }
+
+    def denom(self, rule, children):
+        s1 = self.denominator[children]
+        rhs = self.rhs[rule]
+        s2 = self.cnt_by_rhs[rhs]
+        return s1 + self.prior * s2
 
     def produce(self):
         return self
 
     def score(self, root: int, *children: tuple[int]):
-        if len(children) == 0: return 0
-        return self.probs.get((root, *children), float("inf"))
+        if not (v := self.probs.get((root, *children))) is None:
+            return v
+        return -log(self.prior / self.denom(root, children)) if self.prior else float("inf")
 
 
 class AffineLayer(t.nn.Module):

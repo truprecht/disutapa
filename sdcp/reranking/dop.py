a8f89099734e2c9ab2c78107f8bba87cb1de8ab9
Thomas Ruprecht <thomas.ruprecht@tu-dresden.de>
2023-07-13 22:09:40 +0200

    faster 2dop implementation

--- sdcp/reranking/dop.py
+++ sdcp/reranking/dop.py
@@ -1,6 +1,6 @@
 from collections import Counter, defaultdict
 from dataclasses import dataclass, field
-from typing import Iterable
+from typing import Iterable, Any, Callable
 from discodop.tree import Tree, ImmutableTree
 from math import log
 from itertools import chain, product
@@ -10,15 +10,18 @@ from ..grammar.lcfrs import lcfrs_composition, SortedSet
 
 LcfrsRule = tuple[str, tuple[str, ...], lcfrs_composition]
 
-def into_derivation(tree: Tree) -> tuple[Tree, SortedSet]:
+
+def into_derivation(tree: Tree, integerize: Callable[[Any], int] = None) -> tuple[Tree, SortedSet]:
     if len(tree) == 1 and not isinstance(tree[0], Tree):
-        # return ImmutableTree((tree.label, (), lcfrs_composition("0")), []), SortedSet((tree[0],))
         return None, SortedSet((tree[0],))
-    children, positions = zip(*(into_derivation(c) for c in tree))
+    children, positions = zip(*(into_derivation(c, integerize) for c in tree))
     allpositions = positions[0].union(*positions[1:])
     composition, _ = lcfrs_composition.from_positions(allpositions, positions)
+    rule = (tree.label, tuple(c.label for c in tree), composition)
+    if not integerize is None:
+        rule = integerize(rule)
     return ImmutableTree(
-        (tree.label, tuple(c.label for c in tree), composition),
+        rule,
         children
     ), allpositions
 
@@ -40,24 +43,27 @@ class DopRule:
             yield cls(root, children, (i, grandchildren))
 
 
-def common_fragments(s: Tree, t: Tree) -> Iterable[DopRule]:
+def largest_common_fragment(s: Tree, spos: tuple[int, ...], t: Tree, tpos: tuple[int, ...], tidx: int, excludes: set[tuple[tuple[int, ...], int, tuple[int, ...]]]) -> DopRule:
     if s is None or t is None or not s.label == t.label:
-        return
-    root = s.label
-    child_combinations = (
-        chain((None,), common_fragments(s_, t_))
-        for s_,t_ in zip(s, t)
+        return None
+    excludes.add((spos, tidx, tpos))
+    return ImmutableTree(
+        s.label,
+        [
+            largest_common_fragment(*s_, *t_, tidx, excludes)
+            for s_, t_ in zip(children_with_positions(s, spos, False), children_with_positions(t, tpos, False))
+        ]
     )
-    for children in product(*child_combinations):
-        yield ImmutableTree(root, children)
 
 
 class Dop:
     def __init__(self, training_set: Iterable[Tree], prior: float = 0.1):
+        self.rule2idx = defaultdict(lambda: len(self.rule2idx))
+        self.idx2lhs = []
         derivations: list[Tree] = []
-        rule_index: dict[LcfrsRule, list[tuple[int, tuple[int, ...]]]] = defaultdict(list)
+        rule_index: dict[int, list[tuple[int, tuple[int, ...]]]] = defaultdict(list)
         for tree_index, tree in enumerate(training_set):
-            derivation, _ = into_derivation(tree)
+            derivation, _ = into_derivation(tree, integerize=self.integerize)
             derivations.append(derivation)
             queue = [(derivation, ())]
             while queue:
@@ -67,33 +73,45 @@ class Dop:
 
         dop_fragments = Counter()
         for derivation_index, derivation in enumerate(tqdm(derivations, "extracting dop fragments")):
+            exclude_combinations = set()
             queue = [(derivation, ())]
             while queue:
+                already_counted = set()
                 subderivation, position = queue.pop(0)
-                queue.extend((c, position+(i,)) for i,c in enumerate(subderivation) if not c is None)
-                dop_fragments_in_occurrence = set()
+                queue.extend(children_with_positions(subderivation, position))
                 for other_occurrence in rule_index[subderivation.label]:
-                    if (derivation_index, position) == other_occurrence:
+                    other_tidx, other_position = other_occurrence
+                    if other_tidx >= derivation_index:
+                        break
+                    if (position, other_tidx, other_position) in exclude_combinations:
                         continue
-                    other_occurrence = derivations[other_occurrence[0]][other_occurrence[1]]
+                    other_occurrence = derivations[other_tidx][other_position]
                     assert other_occurrence.label == subderivation.label
-                    for common_fragment in common_fragments(subderivation, other_occurrence):
-                        dop_fragments_in_occurrence.add(common_fragment)
-                dop_fragments.update(dop_fragments_in_occurrence)
+                    fragment = largest_common_fragment(subderivation, position, other_occurrence, other_position, other_tidx, exclude_combinations)
+                    if fragment in already_counted:
+                        continue
+                    already_counted.add(fragment)
+                    dop_fragments[fragment] += 1 if fragment in dop_fragments else 2
 
         self.rules = defaultdict(list)
         denominators = Counter()
         for fragment, c in dop_fragments.items():
-            denominators[fragment.label[0]] += c + prior
+            denominators[self.idx2lhs[fragment.label]] += c + prior
             self.rules[fragment.label].append(fragment)
         self.weights = {
-            fragment: log(denominators[fragment.label[0]] + prior) - log(c + prior)
+            fragment: log(denominators[self.idx2lhs[fragment.label]] + prior) - log(c + prior)
             for fragment, c in dop_fragments.items()
         }
         self.fallback_weight = {
             label: log(denom + prior) - log(prior) if prior else float("-inf")
             for label, denom in denominators.items()
         }
+
+
+    def integerize(self, rule) -> int:
+        if not rule in self.rule2idx:
+            self.idx2lhs.append(rule[0])
+        return self.rule2idx[rule]
 
 
     def match(self, tree: Tree) -> float:
@@ -110,8 +128,12 @@ class Dop:
         return bestidx, besttree
 
 
-def children_with_positions(tree, position):
-    return ((c, position+(i,)) for i,c in enumerate(tree) if not c is None)
+def children_with_positions(tree, position, skip_none: bool = True):
+    return (
+        (c, position+(i,))
+        for i,c in enumerate(tree)
+        if not (skip_none and c is None)
+    )
 
 @dataclass
 class DopTreeParser:
@@ -136,7 +158,7 @@ class DopTreeParser:
         return child_derivations
 
     def fill_chart(self, tree: Tree):
-        derivation, _ = into_derivation(tree)
+        derivation, _ = into_derivation(tree, self.grammar.integerize)
         queue = [(derivation, ())]
         while queue:
             subderivation, position = queue.pop()
@@ -146,7 +168,7 @@ class DopTreeParser:
                 continue
             if not subderivation.label in self.grammar.rules:
                 weight = sum(self.chart[p] for _, p in children_with_positions(subderivation, position))
-                weight += self.grammar.fallback_weight.get(subderivation.label[0], 0)
+                weight += self.grammar.fallback_weight.get(self.grammar.idx2lhs[subderivation.label], 0)
                 if not position in self.chart or self.chart[position] > weight:
                     self.chart[position] = weight
                 continue
--- tests/test_dop_reranking.py
+++ tests/test_dop_reranking.py
@@ -29,55 +29,51 @@ def test_into_derivation():
         assert into_derivation(t) == (d, SortedSet(t.leaves()))
 
 def test_common_fragments():
-    assert set(common_fragments(derivations[0], derivations[1])) == set()
-    assert set(common_fragments(derivations[1], derivations[1])) == set((
-        ImmutableTree(("S", ("A", "C", "S"), lcfrs_composition("012")), [None, None, None]),
-        ImmutableTree(("S", ("A", "C", "S"), lcfrs_composition("012")), [
+    excludes = set()
+    
+    assert largest_common_fragment(derivations[0], (), derivations[1], (1,), 1, excludes) is None
+    assert not excludes
+    
+    assert largest_common_fragment(derivations[1], (3,4,5), derivations[1], (1,2,3,), 1, excludes) == ImmutableTree(("S", ("A", "C", "S"), lcfrs_composition("012")), [
             None,
             None,
             ImmutableTree(("S", ("D",), lcfrs_composition("0")), [
                 None,
             ])
-        ]),
-    ))
+        ])
+    assert excludes == {((3,4,5), 1, (1,2,3)), ((3,4,5,2), 1, (1,2,3,2))}
 
 
 def test_dop():
     grammar = Dop(trees, prior=0)
-    r1 = ("S", ("A", "C", "S"), lcfrs_composition("012"))
-    r2 = ("S", ("D",), lcfrs_composition("0"))
-    assert set(grammar.rules.keys()) == { r1, r2 }
+    r1 = 2
+    r2 = 1
+    assert set(grammar.rules.keys()) == { r1 }
     assert set(grammar.rules[r1]) == { 
-        ImmutableTree(r1, [None, None, ImmutableTree(r2, [None])]),
-        ImmutableTree(r1, [None, None, None])
+        ImmutableTree(r1, [None, None, ImmutableTree(r2, [None])])
     }
-    assert set(grammar.rules[r2]) == { ImmutableTree(r2, [None]) }
     assert grammar.weights == {
-        ImmutableTree(r1, [None, None, ImmutableTree(r2, [None])]): log(6) - log(2),
-        ImmutableTree(r1, [None, None, None]): log(6) - log(2),
-        ImmutableTree(r2, [None]): log(6) - log(2),
+        ImmutableTree(r1, [None, None, ImmutableTree(r2, [None])]): 0,
     }
 
     grammar = Dop(trees, prior=1)
     assert grammar.weights == {
-        ImmutableTree(r1, [None, None, ImmutableTree(r2, [None])]): log(10) - log(3),
-        ImmutableTree(r1, [None, None, None]): log(10) - log(3),
-        ImmutableTree(r2, [None]): log(10) - log(3),
+        ImmutableTree(r1, [None, None, ImmutableTree(r2, [None])]): log(4) - log(3),
     }
     assert grammar.fallback_weight == {
-        "S": log(10)
+        "S": log(4)
     }
 
 
 def test_parsing():
-    r1 = ("S", ("A", "C", "S"), lcfrs_composition("012"))
-    r2 = ("S", ("D",), lcfrs_composition("0"))
+    r1 = 2
+    r2 = 1
     fragments = [
         ImmutableTree(r1, [None, None, ImmutableTree(r2, [None])]),
         ImmutableTree(r1, [None, None, None]),
         ImmutableTree(r2, [None]),
     ]
-    tree = derivations[1]
+    tree = fragments[0]
 
     assert list(DopTreeParser._match(fragments[2], tree[2], (2,))) == []
     assert list(DopTreeParser._match(fragments[0], tree, ())) == []
@@ -89,14 +85,14 @@ def test_parsing():
     parser.fill_chart(trees[1])
 
     assert set(parser.chart.keys()) == {(), (2,)}
-    assert parser.chart[()] == log(6) - log(2)
-    assert parser.chart[(2,)] == log(6) - log(2)
+    assert parser.chart[()] == 0
+    assert parser.chart[(2,)] == float("-inf")
 
     parser = DopTreeParser(Dop(trees, prior=1))
     parser.fill_chart(trees[0])
 
     assert set(parser.chart.keys()) == {(), (0,), (1,), (1,2)}
-    assert parser.chart[()] == parser.chart[(0,)] + parser.chart[(1,)] + log(10) - log(1)
-    assert parser.chart[(0,)] == log(10) - log(1)
-    assert parser.chart[(1,)] == log(10) - log(3)
-    assert parser.chart[(1,2,)] == log(10) - log(3)
+    assert parser.chart[()] == parser.chart[(0,)] + parser.chart[(1,)] + log(4)
+    assert parser.chart[(0,)] == log(4)
+    assert parser.chart[(1,)] == log(4) - log(3)
+    assert parser.chart[(1,2,)] == log(4)

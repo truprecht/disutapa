19552e0cb3f4b6bed0e5160cd0001743314bc0ab
Thomas Ruprecht <thomas.ruprecht@tu-dresden.de>
2023-09-28 10:43:00 +0200

    add arg variables to grid

--- sdcp/cli/grid.py
+++ sdcp/cli/grid.py
@@ -6,6 +6,13 @@ import yaml
 
 def main(args):
     yamlfile = yaml.safe_load(args.gridfile)
+    constants = {}
+    inputs = dict(a.split("=", 1) for a in args.constants)
+    for constant in yamlfile.get("constants", []):
+        if not constant in inputs:
+            print(f"missing value for {constant} add it as parameter of the form {constant}=VALUE")
+            exit(1)
+        constants[constant] = inputs[constant]
     variables_and_values = yamlfile.get("variables", {})
     subcommands = yamlfile.get("commands", [])    
     variables = variables_and_values.keys()
@@ -15,6 +22,8 @@ def main(args):
         for command in subcommands:
             for (var,value) in zip(variables,point):
                 command = command.replace(f"[{var}]", value)
+            for cname, cvalue in constants.items():
+                command = command.replace(f"[{cname}]", cvalue)
             if not command in already_executed:
                 already_executed.add(command)
                 print("executing:", command)
@@ -23,4 +32,5 @@ def main(args):
 
 def subcommand(sub: ArgumentParser):
     sub.add_argument("gridfile", type=open)
+    sub.add_argument("constants", type=str, nargs="*")
     sub.set_defaults(func=lambda args: main(args))
